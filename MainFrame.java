a3aae53ad93d3abab243f70a9cdfd14c0a8dc2ab
Jeesoo Min <34534423+kuongee@users.noreply.github.com>
2017-12-20 23:47:58 +0900

    Client program

---
+++ MainFrame.java
@@ -0,0 +1,45 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+public class MainFrame extends JFrame {
+	
+	static Toolkit toolkit = Toolkit.getDefaultToolkit();
+	
+	ClientFrame cf;
+	
+	OGamePanel gp; 
+	OScorePanel sp;
+
+	
+	public MainFrame(ClientFrame c){
+		super("Othello");
+		this.cf = c;
+		sp = new OScorePanel();
+		gp = new OGamePanel(sp, cf);
+		makingFrame();
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//setResizable(false);		
+		//setVisible(true);
+	}
+	
+	public void makingFrame(){
+		Container contain = this.getContentPane();
+		setLayout(null);
+		setSize(gp.wh+390, gp.wh+70);
+		contain.setBackground(new Color(219,219,219));
+		add(gp);
+		add(sp);
+	}
+	
+	/*public static void main(String args[]){
+		new MainFrame();
+	}
+	*/
+	
+	
+}
---
+++ OStone.java
@@ -0,0 +1,64 @@
+import java.awt.*;
+
+
+public class OStone {
+	
+	private int sizeofStone;
+	private int xofStone;
+	private int yofStone;
+	private Color color;
+	private int myStone = 0;	//1은 흰 돌 -1은 검은 돌
+	
+	private int numberofStone = 32;
+	
+	public OStone(){
+		myStone = 0;
+	}
+	
+	public void setColorofStone(){
+		if(myStone == 1)
+			color = Color.white;
+		else if(myStone == -1)
+			color = Color.black;
+	}
+	public Color getColorofStone(){
+		return color;
+	}
+	
+	public void setMystone(int s){
+		myStone = s;
+		setColorofStone();
+	}
+	public int getMystone(){
+		return myStone;
+	}
+	
+////돌의 크기와 x,y좌표와 색
+	public void setXofStone(int x){
+		xofStone = x+10;
+	}
+	public void setYofStone(int y){
+		yofStone = y+10;
+	}
+	public void setSizeofStone(int s){
+		sizeofStone = s-20;
+	}
+	
+	public int getXofStone() {
+		return xofStone;
+	}
+	public int getYofStone() {
+		return yofStone;
+	}
+	public int getSizeofStone() {
+		return sizeofStone;
+	}
+
+	public int getNumberofStone() {
+		return numberofStone;
+	}
+	public void setNumberofStone(int numberofStone) {
+		this.numberofStone = numberofStone;
+	}
+
+}
